7cf7902399369a85df8091e30b3b6801af47c609
Geekles <tondjilee@gmail.com>
2022-07-01 05:39:03 +0300

    main page built

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,7 +2,21 @@ import React, {memo} from "react";
 import styled from "styled-components";
 
 const HomeWrapper = styled.div`
-
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  
+  p, h1 {
+    color: #fff;
+    text-align: center;
+  }
+  
+  p {
+    margin-top: 0;
+  }
+  
 `;
 
 interface HomeProps {
@@ -12,7 +26,8 @@ interface HomeProps {
 const Home = ({}: HomeProps) => {
 
     return <HomeWrapper>
-        Home
+        <h1>Hooks bible</h1>
+        <p>Hey, welcome :) you can find some pre-written react hooks ready to use.</p>
     </HomeWrapper>
 
 }
